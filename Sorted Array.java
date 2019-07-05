93ada5136b59b4cbf302672bcff5c32bb53d5a30
Sehaj grover <sehaj131@gmail.com>
2019-07-05 12:31:15 +0530

    complete

--- Sorted Array.java
+++ Sorted Array.java
@@ -1,20 +1,49 @@
 class Solution {
     public int search(int[] nums, int target) {
     
-    	int i=0;
-    	while(nums[i]<=target) 
-    	{
-    		if(nums[i]==target) {return i;}
-    		i++;
-    	}
-    	return -1;
+        int start=0,end=1;
+        int mid;
+        try{
+        while(nums[end]<target)
+        {
+            start=end;
+            end*=2;
+        }
+        return binarySearch(nums, start, end, target);
+        }
+        catch(Exception e)
+        {
+            try {
+            while(nums[start]<=target) 
+            {
+                if(nums[start]==target){return start;}
+                start++;
+            }
+            return -1;
+            }
+            catch(Exception i) {return -1;}
+        }
+        
     }
-
+    public  int binarySearch(int[] arr,int start,int end, int target) 
+    {
+        int mid;
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            
+            if(arr[mid]==target) {return mid;}
+            else if(arr[mid]<target) {start=mid+1;}
+            else{end=mid-1;}
+        }
+        return -1;
+        
+    }
     public static void main(String args[]) 
     { 
         int arr[] = {1,2,3,4,5,5,6,6,7,8};
         Solution s = new Solution();
-        System.out.println(s.search(arr,6));
+        System.out.println(s.search(arr,8));
         
     } 
 }
